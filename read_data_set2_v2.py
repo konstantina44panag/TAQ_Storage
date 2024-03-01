111cb37119ccc155e58ecd3a3983e4cef31c06dd
Konstantina Panagopoulou <151877044+konstantina44panag@users.noreply.github.com>
2024-03-01 09:42:38 +0200

    Create read_data_set2_v2.py

---
+++ read_data_set2_v2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import h5py
+
+# Path to your HDF5 file
+hdf5_file_path = '200701_v2.h5'  
+
+with h5py.File(hdf5_file_path, 'r') as f:
+    # List of dataset paths you want to examine
+    dataset_paths = ['MSFT/03/ctm/table', 'MSFT/03/complete_nbbo/table']
+
+    for dataset_path in dataset_paths:
+        if dataset_path in f:
+            dataset = f[dataset_path]
+            data = dataset[:] 
+            print(f"Successfully accessed dataset at path: {dataset_path}")
+        else:
+            print(f"Dataset at path {dataset_path} not found")
